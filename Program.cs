6c09e2e9602bb6fe62546702770efa8a02526ed8
JJ <jh_86@hotmail.co.uk>
2021-06-01 11:37:51 +0100

    project console set up

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcampgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("Bootcamp War");
+            Console.WriteLine("What is your name?");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+A simple commandline game made  in C# to have a fictional story based on my experiences with code. 
+Just a fun little game to be enjoyed.
